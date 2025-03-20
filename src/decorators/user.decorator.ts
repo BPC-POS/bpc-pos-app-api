9eab06cc18d099cd5885ba39050c70544b93e5cd
Y Bin <ybin.nguyen@estuary.solutions>
2025-03-21 02:39:51 +0700

    refactor: simplify parameter handling in user decorators

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -1,12 +1,12 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const ReqUser = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+export const ReqUser = createParamDecorator((_, ctx: ExecutionContext) => {
 	const request = ctx.switchToHttp().getRequest();
 	return request['state'].user;
 });
 
 export const ReqSystemConfigInformation = createParamDecorator(
-	(data: unknown, ctx: ExecutionContext) => {
+	(_, ctx: ExecutionContext) => {
 		const request = ctx.switchToHttp().getRequest();
 		return request['state'].systemConfig;
 	},
